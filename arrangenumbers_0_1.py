8e6fec021c65bd241961d4c37cb31d0469141799
Veera Määttänen <38630359+veerux@users.noreply.github.com>
2021-01-11 19:27:15 +0200

    Add files via upload

---
+++ arrangenumbers_0_1.py
@@ -0,0 +1,10 @@
+# ArrangeNumbers -game 
+# @Veera Määttänen
+
+print("8 puzzle - arrange the numbers by swapping with the empty place")
+number = input("give a number: ")
+print("the number is " + number)
+
+
+
+
