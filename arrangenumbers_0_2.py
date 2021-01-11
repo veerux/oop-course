224366eb1a3e030b97d3c925e832c3deb50636ed
Veera Määttänen <38630359+veerux@users.noreply.github.com>
2021-01-11 20:19:08 +0200

    Create arrangenumbers_0_2.py

---
+++ arrangenumbers_0_2.py
@@ -0,0 +1,10 @@
+# Arrange Numbers -game 
+# @Veera Määttänen
+
+print("8 puzzle - arrange the numbers by swapping with the empty place")
+number = input("give a number: ")
+print("the number is " + number)
+
+
+
+
