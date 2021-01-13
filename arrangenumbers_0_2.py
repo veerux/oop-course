586bbca9751f189bf039c63e4a177ecf5839360e
Veera Määttänen <38630359+veerux@users.noreply.github.com>
2021-01-13 15:51:25 +0200

    Update arrangenumbers_0_2.py

--- arrangenumbers_0_2.py
+++ arrangenumbers_0_2.py
@@ -1,10 +1,33 @@
-# Arrange Numbers -game 
-# @Veera Määttänen
+'''
+Arrange Numbers -game where the user tries to arrange the numbers in a
+3x3 table so that they are sorted from smallest to largest. 
+@author: Veera Määttänen
+'''
+
+
+def swapPositions(numbers, pos1, pos2):
+    numbers[pos1], numbers[pos2] = numbers[pos2], numbers[pos1]
+    return numbers
+
+numbers = ['4', '7', '2', '6', '8', '3', '5', '_', '1']
+check = ('1', '2', '3', '4', '5', '6', '7', '8', '_' )
+
 
 print("8 puzzle - arrange the numbers by swapping with the empty place")
-number = input("give a number: ")
-print("the number is " + number)
+print(numbers)
 
 
+
+while tuple(numbers) != check : 
+    number = input("number to move: ")
+    first = numbers.index(number)
+    second = numbers.index('_')
+    swapPositions(numbers, first, second)
+    print(numbers)
+
+
+
+print(numbers)
+print("The numbers are in the correct order")
 
 
